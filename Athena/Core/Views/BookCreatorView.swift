ae17ad6247c976d1ff7e1fcbb707d1e351c251c2
Lautaro Bonasora <lautaro.bonasora100@gmail.com>
2023-12-21 08:12:06 -0300

    new book creator

--- Athena/Core/Views/BookCreatorView.swift
+++ Athena/Core/Views/BookCreatorView.swift
@@ -8,13 +8,77 @@
 import SwiftUI
 
 struct BookCreatorView: View {
+    @State private var title = ""
+    @State private var author = ""
+    @State private var ISBN = ""
+    @State private var publicationYear = ""
+    @State private var genre = "Fantasy"
+    @State private var publisher = ""
+    @State private var url = ""
+    
+    
+    let bookGenres = ["Fantasy", "Mystery", "Romance", "Science Fiction", "Thriller", "Non-fiction"]
+    
     var body: some View {
-        Text("Hallo Test!")
+        NavigationView {
+            
+            Form {
+                Section {
+                    TextField("Titulo", text: $title)
+                    TextField("Autor", text: $author)
+                    TextField("Año de Publicacion", text: $publicationYear).keyboardType(.numberPad)
+                    Picker("Genero", selection: $genre) {
+                        ForEach(bookGenres, id: \.self) { genre in
+                                        Text(genre)
+                                    }
+                    }
+                }
+                
+                Section {
+                    TextField("Titulo", text: $title)
+                    TextField("Autor", text: $author)
+                    TextField("Año de Publicacion", text: $publicationYear).keyboardType(.numberPad)
+                    Picker("Genero", selection: $genre) {
+                        ForEach(bookGenres, id: \.self) { genre in
+                                        Text(genre)
+                                    }
+                    }
+                }
+                
+                Section {
+                    TextField("Titulo", text: $title)
+                    TextField("Autor", text: $author)
+                    TextField("Año de Publicacion", text: $publicationYear).keyboardType(.numberPad)
+                    Picker("Genero", selection: $genre) {
+                        ForEach(bookGenres, id: \.self) { genre in
+                                        Text(genre)
+                                    }
+                    }
+                }
+                                
+                Section {
+                    Button(action: register) {
+                      Text("Registrar")
+                    }
+                    .buttonStyle(.automatic)
+                }
+              
+                
+
+            }
+        }
+    }
+    
+    func register() {
+        
     }
 }
 
 struct BookCreatorView_Previews: PreviewProvider {
     static var previews: some View {
-        BookCreatorView()
+        NavigationView {
+            BookCreatorView()
+        }
+        
     }
 }
