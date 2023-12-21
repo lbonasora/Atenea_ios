a288f74386104b896404707ae42a8be55e73d438
Lautaro Bonasora <lautaro.bonasora100@gmail.com>
2023-12-21 06:13:51 -0300

    Testing new branch

---
+++ Athena/Core/Views/BookCreatorView.swift
@@ -0,0 +1,20 @@
+//
+//  BookCreatorView.swift
+//  Athena
+//
+//  Created by Lautaro Bonasora on 21/12/2023.
+//
+
+import SwiftUI
+
+struct BookCreatorView: View {
+    var body: some View {
+        Text("Hallo Test!")
+    }
+}
+
+struct BookCreatorView_Previews: PreviewProvider {
+    static var previews: some View {
+        BookCreatorView()
+    }
+}
